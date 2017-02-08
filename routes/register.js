5913eb30733dcb4045036dc4624a05e42a78f2b3
Ryan Mulcahy <ryan@primeacademy.io>
2017-02-08 11:46:19 -0600

    Add register route

---
+++ routes/register.js
@@ -0,0 +1,32 @@
+var router = require('express').Router();
+var User = require('../models/user');
+
+router.post('/', function(req, res){
+  User.findOne({ username: req.body.username }, function(err, user){
+    if (err) {
+      console.log('Error checking for existing user', err);
+      return res.sendStatus(500);
+    }
+
+    if (user) {
+      return res.status(400).send('Username already taken');
+    }
+
+    var user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    user.save(function(err){
+      if (err) {
+        console.log('Error saving new user', err);
+        res.sendStatus(500);
+      } else {
+        console.log('Created new user');
+        res.sendStatus(201);
+      }
+    });
+  });
+});
+
+module.exports = router;
